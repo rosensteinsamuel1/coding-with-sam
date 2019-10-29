1860c71afe38fe762cbbff532bc23830d8873bdd
rosenstein.samuel1 <rosenstein.samuel1@gmail.com>
2019-10-29 19:13:37 +0200

    commit

---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import styles from './Homepage.module.css'
+
+class Homepage extends Component {
+    render() {
+        return(
+            <div className={styles.Homepage}>
+                <h1>Welcome to my site</h1>
+
+            </div>
+        )
+    }
+}
+
+export default Homepage;
